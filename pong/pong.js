bedda8548538fe1e8d8bab4d3125374801b113f9
lukaszpetra <lukaszpetra@gmail.com>
2020-04-14 23:48:52 +0200

    modified pong/pong.js

--- pong/pong.js
+++ pong/pong.js
@@ -3,5 +3,39 @@ const cvs = document.getElementById("pong");
 const ctx = cvs.getContext("2d");
 
 //draw rect
+function drawRect(x,y,w,h, color){
+    
+    ctx.fillStyle = color;
+    ctx.fillRect(x,y,w,h);
+    
+}
+
+drawRect(0,0,cvs.width, cvs.height, "black");
+
+//draw Ball
+
+function drawBall(x,y,r,color){
+    
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(x,y,r,0,Math.PI*2,false);
+    ctx.closePath();
+    ctx.fill();
+}
+
+drawBall(100,100,50,"white");
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
